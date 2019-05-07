413f8c52031b38688dfbd4091dff9f819b10d6d9
Emmanuel idehen <23498282+emmanuelidehen@users.noreply.github.com>
2019-05-07 10:54:51 -0500

    Add files via upload

--- Human.java
+++ Human.java
@@ -1,4 +1,52 @@
 
-public class Human {
+import java.io.*;
+import java.awt.print.*;
+import java.util.stream.*;
+import java.util.function.*;
 
+
+public class Human {
+	String name = null;
+	int age;
+	int height;
+	
+	public Human()
+	{
+		
+	}
+	
+	public void spaek()
+	{
+		System.out.println("Hello my name is "+ name);
+		System.out.println("my age is "+ age);
+		System.out.println("I am  "+ height + " cm tall");
+	}
+
+	public void jump()
+	{
+		System.out.println("Hey look I can jump ");	
+	}
+	public void eating()
+	{
+		System.out.println("Hey "+ name + "look i'm eating");
+	}
+	
+	public static void main() {
+		
+		Human emma = null;
+		
+		
+		emma.age = 24;
+		emma.height = 100;
+		emma.name = "emmanuel";
+		
+		emma.spaek();
+		emma.jump();
+		emma.eating();
+		
+		
+		
+		
+	}
+	
 }
